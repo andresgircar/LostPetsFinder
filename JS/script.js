a34216681e7817f54eb9fad2abf71dc438a3b877
Andrés Giraldo <andresgircar@gmail.com>
2022-10-09 23:30:11 -0500

    adding styles

--- JS/script.js
+++ JS/script.js
@@ -1,16 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.fixed-action-btn');
-    var instances = M.FloatingActionButton.init(elems, {
+    var instance = M.FloatingActionButton.init(elems, {
     direction: 'left',
     hoverEnabled: false
     });
+    instance.open();
+    instance.close();
+    instance.destroy();
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.datepicker');
-    var instances = M.Datepicker.init(elems, options);
-    });
 
-instance.open();
-instance.close();
-instance.destroy();
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var date = document.querySelectorAll('.datepicker');
+    var instance = M.Datepicker.init(date, {defaultDate:null, autoClose: true});
+    instance.open();
+    instance.close();
+    instance.destroy();
+});
+
+
+$(document).ready(function() {
+    $('input#input_text, textarea#textarea2').characterCounter();
+});
